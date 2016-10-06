4400c2c07b816e35688f721e41396090d98f8367
Daniel Pradilla <info@danielpradilla.info>
2016-10-06 16:53:18 +0200

    common api

--- or-tools_api.py
+++ or-tools_api.py
@@ -4,7 +4,9 @@
 
 import falcon
 import json
-import room_assignments_json as raj 
+
+from interview_grocery_startup import interview_grocery_startup_json as igsj 
+from room_assignments import room_assignments_json as raj 
 
 
 ALLOWED_ORIGINS = 'http://localhost'
@@ -15,6 +17,30 @@ class CorsMiddleware(object):
 		if origin in ALLOWED_ORIGINS:
 			response.set_header('Access-Control-Allow-Origin', origin)
 		response.set_header('Access-Control-Allow-Origin', '*')
+
+class InterviewGroceryStartupResource(object):
+    def on_get(self, req, resp):
+		#Handles GET requests
+		
+
+		resp.status = falcon.HTTP_200  # This is the default status
+
+		resp.body =  igsj.main(cfg,cfg['what'])
+
+    def on_post(self, req, resp):
+		try:
+			body = req.stream.read()
+			body_json = json.loads(body.decode('utf-8'))
+			print(body)
+			cfg = body_json["cfg"]
+		except KeyError:
+			raise falcon.HTTPBadRequest(
+			'Missing Config',
+			'A config (cfg) must be submitted in the request body.')
+
+		resp.status = falcon.HTTP_200
+		resp.body = igsj.main(cfg,cfg['what'])
+
 
 class RoomAssignmentsResource(object):
     def on_get(self, req, resp):
@@ -45,11 +71,17 @@ class RoomAssignmentsResource(object):
 app = application = falcon.API(middleware=[CorsMiddleware()])
 
 # Resources are represented by long-lived class instances
+igsapi = InterviewGroceryStartupResource()
 room_assignments_api = RoomAssignmentsResource()
 
 # ApiTestResource will handle all requests to the '/apitest' URL path
+app.add_route('/igsapi', igsapi)
 app.add_route('/room_assignments_api', room_assignments_api)
 
+
+# Resources are represented by long-lived class instances
+
+
 # Run as
-#gunicorn room_assignments_api -b :18000 --reload
+#gunicorn or-tools_api -b :18000 --reload
 
